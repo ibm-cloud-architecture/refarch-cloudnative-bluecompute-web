2c494ecf075582b4241ad4345ba3f1d489924ea1
Gang Chen <gangchen@us.ibm.com>
2016-11-21 08:47:00 -0600

    Add test case for BlueCompute Web
    Signed-off-by: Gang Chen <gangchen@us.ibm.com>

---
+++ StoreWebApp/test/storeapp-test.js
@@ -0,0 +1,49 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+//const chaiJquery  = require('chai-jquery')
+const request = require('request');
+
+// Register the plugin
+//chai.use(chaiJquery);
+
+var cfenv = require('cfenv');
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+describe('BlueCompute Web App Test Suites', function() {
+
+
+	//console.log(process.env);
+	//console.log("################");
+	//console.log(appEnv);
+	var serviceBaseUrl = "http://localhost:8000";
+	if (process.env.test_env == 'cloud') {
+		serviceBaseUrl = 'http://' + appEnv.name + '.mybluemix.net';
+	}
+
+	// test the home page loading
+	describe('when request the home page', function() {
+		it('should return valid HTML page', function(done) {
+
+			var endPoint = serviceBaseUrl;
+
+      request(endPoint, function(error, response, body){
+
+				  //console.log(body);
+
+          // Parse the home page HTML and expect the right content
+				  expect(error).to.not.be.ok;
+          expect(error).to.be.null;
+				  expect(response).to.have.property('statusCode', 200);
+          expect(response).to.be.html;
+          expect(body).to.include('BlueCompute Store!');
+          expect(body).to.include('<!DOCTYPE html>');
+          expect(body).to.include('IBM Cloud Architecture Center');
+
+				  done();
+			});
+		});
+	});
+
+
+});
