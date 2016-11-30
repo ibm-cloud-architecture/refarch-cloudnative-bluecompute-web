4d1533a8603ad6817fa545ddf6dbd97a86b44670
jkwong888 <jkwong@ca.ibm.com>
2016-11-30 15:46:19 -0500

    Revert "MCA auth for index"
    This reverts commit b24b9ac6cd695e19809e46413337e20e8f962d1b.

--- StoreWebApp/routes/index.js
+++ StoreWebApp/routes/index.js
@@ -1,20 +1,9 @@
 var express = require('express');
-var passport = require('passport');
-
 var router = express.Router();
 
-
 /* GET home page. */
-router.get('/', passport.authenticate('mca-website-strategy', {session: false }),
-    function(req, res){
-      res.render('index', {title: 'ThinkIBM Consumer'});
-    }
-);
-
-router.get('/authenticated', function (req, res) {
+router.get('/', function (req, res) {
   res.render('index', {title: 'ThinkIBM Consumer'});
 });
-
-
 
 module.exports = router;
