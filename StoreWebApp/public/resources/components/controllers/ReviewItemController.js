85d9147496221391848c8fba53e7826a05c74fc3
Chris Tchoukaleff <cptchouk@us.ibm.com>
2017-02-16 20:56:24 -0600

    Added stars to review page
    Signed-off-by: Chris Tchoukaleff <cptchouk@us.ibm.com>

--- StoreWebApp/public/resources/components/controllers/ReviewItemController.js
+++ StoreWebApp/public/resources/components/controllers/ReviewItemController.js
@@ -4,18 +4,25 @@ app.controller('ReviewItemController', ['$scope','$routeParams','UserInfoService
 
 	$scope.loggedIn = UserInfoService.authenticated
 
+	angular.element('#stars').starrr();
+
+	$scope.getStars = function () {
+			$scope.count = angular.element('#stars').find('.fa-star.fa');
+	}
+
   $scope.addReview = function () {
 
   			$scope.payload = {
                           'reviewer_name':$scope.reviewer_name,
   												'reviewer_email':$scope.reviewer_email,
-                          'rating':$scope.rating,
+                          'rating':$scope.count.length,
                           'comment':$scope.comments
   											}
 
   			BlueAPIService.addReviewItem(UserInfoService.accessToken, $routeParams.id, $scope.payload, function (response) {
   					console.log("Add Review Item Result" + response)
   					$scope.result = response.data
+						$location.path('item/'+$routeParams.id);
 
   				}, function (error){
   					console.log("Add Review Item Error: " + error);
