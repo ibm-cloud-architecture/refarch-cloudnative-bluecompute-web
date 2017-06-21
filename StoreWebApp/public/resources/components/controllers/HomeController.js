19ddf674025664410c7e1275f5994ada1d55d053
Fabio Gomez <fabiogomez@us.ibm.com>
2017-06-21 18:14:31 -0400

    Progress for displaying region

--- StoreWebApp/public/resources/components/controllers/HomeController.js
+++ StoreWebApp/public/resources/components/controllers/HomeController.js
@@ -1,7 +1,17 @@
-app.controller('HomeController', ['$scope','UserInfoService',function($scope, UserInfoService) {
+app.controller('HomeController', ['$scope','UserInfoService',function($scope, UserInfoService, CONFIG) {
 
 	console.log("Entering Home Controller")
 
 	$scope.loggedIn = UserInfoService.state.authenticated
+	var cluster_name = CONFIG.Application.cluster_name;
+	var region = CONFIG.Application.region;
+
+	if (cluster_name && cluster_name !== "") {
+		$scope.clusterAndRegion = ("\nHosted in cluster \"" + cluster_name + "\"");
+	}
+
+	if (region && region !== "") {
+		$scope.clusterAndRegion += (" in region \"" + region + "\"");
+	}
 
 }]);
