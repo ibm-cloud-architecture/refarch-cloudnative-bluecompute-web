86fa9895a2ed0e7b3af5ef85f246f0e2834e50ff
jkwong888 <jkwong@ca.ibm.com>
2017-07-27 20:23:51 +0800

    add order history to the customer profile page

--- StoreWebApp/public/resources/components/controllers/CustomerController.js
+++ StoreWebApp/public/resources/components/controllers/CustomerController.js
@@ -10,6 +10,35 @@ app.controller('CustomerController', ['$scope','$routeParams','$location','UserI
 
 	 }, function (error){
 		 console.log("Customer Profile Error: " + error);
- });
+
+    });
+
+    var catalogMap = {};
+    BlueAPIService.getCatalog(function (response) {
+        var catalog = response.data;
+        for (let i = 0; i < catalog.length; i++) {
+            var cat = catalog[i];
+            catalogMap[cat.id] = cat.name;
+        }
+
+        BlueAPIService.getCustomerOrders(UserInfoService.state.accessToken, function (response) {
+            console.log("Get Orders Result" + response.data)
+            var ordersData = response.data;
+            var ordersInfo = [];
+            for (let i = 0; i < ordersData.length; i++) {
+                let o = ordersData[i];
+                ordersInfo.push({date: o.date, itemId: o.itemId, itemName: catalogMap[o.itemId], count: o.count});
+            }
+            $scope.ordersInfo = ordersInfo;
+        }, function (error){
+            console.log("Get Orders Error: " + error);
+        });
+	}, function (error){
+		 console.log("Get Catalog Error: " + error);
+    });
+
+
+
+
 
 }]);
