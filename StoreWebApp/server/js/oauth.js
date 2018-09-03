df04f218995e7b47ac23ed3d08a60bf08cf07fd8
Fabio Gomez <fabiogomez@us.ibm.com>
2018-09-03 13:07:50 -0500

    - Fixed linting errors and beautified routes code

--- StoreWebApp/server/js/oauth.js
+++ StoreWebApp/server/js/oauth.js
@@ -10,74 +10,73 @@ var _apiServerOrg = ((_apiServer.org == "") || (typeof _apiServer.org == 'undefi
 var _apiServerCatalog = ((_apiServer.catalog == "") || (typeof _apiServer.catalog == 'undefined')) ? undefined : _apiServer.catalog;
 var _apis = config.get('APIs');
 
-module.exports.login = function (username, password, session) {
+module.exports.login = function(username, password, session) {
 
-  console.log("inside of oauth.js");
-  return new Promise(function (fulfill, reject) {
-    if (typeof session.oauth2token !== 'undefined') {
-      console.log("Using OAuth Token: " + session.oauth2token);
-      fulfill(session.oauth2token);
-    }
-    else {
+    console.log("inside of oauth.js");
+    return new Promise(function(fulfill, reject) {
+        if (typeof session.oauth2token !== 'undefined') {
+            console.log("Using OAuth Token: " + session.oauth2token);
+            fulfill(session.oauth2token);
+        } else {
 
-      var authz_url = api_url.stringify({
-        protocol: _apiServer.protocol,
-        host: _apiServer.host,
-        org: _apiServerOrg,
-        cat: _apiServerCatalog,
-        api: _apis.oauth20.base_path,
-        operation: _apis.oauth20.paths.authz
-      });
-
-
-
-      var token_url = api_url.stringify({
-        protocol: _apiServer.protocol,
-        host: _apiServer.host,
-        org: _apiServerOrg,
-        cat: _apiServerCatalog,
-        api: _apis.oauth20.base_path,
-        operation: _apis.oauth20.paths.token
-      });
-
-
-      console.log("Invoke OAuth endpoint: " + JSON.stringify(token_url));
-    /*  console.log("Invoke OAuth endpoint: " + JSON.stringify(_myApp.client_id));
-      console.log("Invoke OAuth endpoint: " + JSON.stringify(_myApp.client_secret));
-      console.log("Invoke OAuth endpoint: " + JSON.stringify(authz_url));
-      console.log("Invoke OAuth endpoint: " + JSON.stringify(_apis.oauth20.grant_types));
-      console.log("Invoke OAuth endpoint: " + JSON.stringify(_apis.oauth20.redirect_url));
-      console.log("Invoke OAuth endpoint: " + JSON.stringify(_apis.oauth20.scopes)); */
-
-      var thinkAuth = new ClientOAuth2({
-        clientId: _myApp.client_id,
-        clientSecret: _myApp.client_secret,
-        accessTokenUri: token_url,
-        authorizationUri: authz_url,
-        authorizationGrants: _apis.oauth20.grant_types,
-        redirectUri: _apis.oauth20.redirect_url,
-        scopes: _apis.oauth20.scopes
-      });
+            var authz_url = api_url.stringify({
+                protocol: _apiServer.protocol,
+                host: _apiServer.host,
+                org: _apiServerOrg,
+                cat: _apiServerCatalog,
+                api: _apis.oauth20.base_path,
+                operation: _apis.oauth20.paths.authz
+            });
 
 
 
-      // Set an option to disable the check for self-signed certificates
-      var options = {
-        options: {
-          rejectUnauthorized: false
+            var token_url = api_url.stringify({
+                protocol: _apiServer.protocol,
+                host: _apiServer.host,
+                org: _apiServerOrg,
+                cat: _apiServerCatalog,
+                api: _apis.oauth20.base_path,
+                operation: _apis.oauth20.paths.token
+            });
+
+
+            console.log("Invoke OAuth endpoint: " + JSON.stringify(token_url));
+            /*  console.log("Invoke OAuth endpoint: " + JSON.stringify(_myApp.client_id));
+              console.log("Invoke OAuth endpoint: " + JSON.stringify(_myApp.client_secret));
+              console.log("Invoke OAuth endpoint: " + JSON.stringify(authz_url));
+              console.log("Invoke OAuth endpoint: " + JSON.stringify(_apis.oauth20.grant_types));
+              console.log("Invoke OAuth endpoint: " + JSON.stringify(_apis.oauth20.redirect_url));
+              console.log("Invoke OAuth endpoint: " + JSON.stringify(_apis.oauth20.scopes)); */
+
+            var thinkAuth = new ClientOAuth2({
+                clientId: _myApp.client_id,
+                clientSecret: _myApp.client_secret,
+                accessTokenUri: token_url,
+                authorizationUri: authz_url,
+                authorizationGrants: _apis.oauth20.grant_types,
+                redirectUri: _apis.oauth20.redirect_url,
+                scopes: _apis.oauth20.scopes
+            });
+
+
+
+            // Set an option to disable the check for self-signed certificates
+            var options = {
+                options: {
+                    rejectUnauthorized: false
+                }
+            };
+
+            thinkAuth.owner.getToken(username, password, options)
+                .then(function(user) {
+                    session.oauth2token = user.accessToken;
+                    console.log("Using OAuth Token: " + session.oauth2token);
+                    fulfill(session.oauth2token);
+                })
+                .catch(function(reason) {
+                    reject(reason);
+                });
         }
-      };
-
-      thinkAuth.owner.getToken(username, password, options)
-        .then(function (user) {
-          session.oauth2token = user.accessToken;
-          console.log("Using OAuth Token: " + session.oauth2token);
-          fulfill(session.oauth2token);
-        })
-        .catch(function (reason) {
-          reject(reason);
-        });
-    }
-  });
+    });
 
 };
