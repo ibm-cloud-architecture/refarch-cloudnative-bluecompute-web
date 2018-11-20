e4993429eecabd72344cd4baa08549ca8930a51a
Fabio Gomez <fabiogomez@us.ibm.com>
2018-11-19 23:24:58 -0600

    - Simplified generate_load.sh script

--- scripts/generate_load.sh
+++ scripts/generate_load.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
-HOST="localhost";
-PORT="8000";
-URL="http://${HOST}:${PORT}";
-DEPLOYMENT="web-web";
+URL="$1";
 SERVICE_PATH="catalog";
 
-# trap ctrl-c and call ctrl_c() to stop port forwarding
-trap ctrl_c INT
-
-function ctrl_c() {
-	echo "** Trapped CTRL-C... Killing Port Forwarding and Stopping Load";
-	killall kubectl;
-	exit 0;
-}
-
-function start_port_forwarding() {
-	echo "Forwarding service port ${PORT}";
-	kubectl port-forward deployment/${DEPLOYMENT} ${PORT}:${PORT} --pod-running-timeout=1h &
-	echo "Sleeping for 3 seconds while connection is established...";
-	sleep 3;
-}
-
-# Port Forwarding
-start_port_forwarding
+if [ -z "$URL" ]; then
+	URL="http://localhost:8080"
+	echo "No URL provided! Using ${URL}"
+fi
 
 # Load Generation
 echo "Generating load..."
 
 while true; do
 	curl -s ${URL}/${SERVICE_PATH} > /dev/null;
+	echo -n .;
 	sleep 0.2;
 done
