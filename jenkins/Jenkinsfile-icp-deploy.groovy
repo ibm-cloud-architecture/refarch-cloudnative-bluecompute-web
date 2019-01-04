fa413205464a33052af6ec72bd28f8b86a75900a
Fabio Gomez <fabiogomez@us.ibm.com>
2019-01-04 08:07:22 -0600

    - Took out namespace in podTemplate

--- jenkins/Jenkinsfile-icp-deploy.groovy
+++ jenkins/Jenkinsfile-icp-deploy.groovy
@@ -47,7 +47,7 @@ def ordersPort = env.ORDERS_PORT ?: "8084"
  */
 def helmHome = env.HELM_HOME ?: env.JENKINS_HOME + "/.helm"
 
-podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, namespace: namespace, envVars: [
+podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, envVars: [
         envVar(key: 'CLUSTER_URL', value: clusterURL),
         envVar(key: 'CLUSTER_ACCOUNT_ID', value: clusterAccountId),
         envVar(key: 'NAMESPACE', value: namespace),
