bd53a4316023bb699a2da8deac44d1f1a9e164e6
Fabio Gomez <fabiogomez@us.ibm.com>
2019-01-07 10:47:05 -0600

    - Trying just the dind container

--- jenkins/Jenkinsfile-build.groovy
+++ jenkins/Jenkinsfile-build.groovy
@@ -76,8 +76,8 @@ podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, envVa
     ],
     containers: [
         containerTemplate(name: 'nodejs', image: 'ibmcase/nodejs:6-alpine', ttyEnabled: true, command: 'cat'),
-        containerTemplate(name: 'docker' , image: 'ibmcase/docker:18.09', ttyEnabled: true, command: 'cat', envVars: [envVar(key: 'DOCKER_HOST', value: dockerHost)]),
-        containerTemplate(name: 'docker-in-docker', image: 'docker:18.09-dind', privileged: true),
+        //containerTemplate(name: 'docker' , image: 'ibmcase/docker:18.09', ttyEnabled: true, command: 'cat', envVars: [envVar(key: 'DOCKER_HOST', value: dockerHost)]),
+        containerTemplate(name: 'docker', image: 'ibmcase/docker:18.09-dind', privileged: true),
         containerTemplate(name: 'kubernetes', image: 'ibmcase/jenkins-slave-utils:1', ttyEnabled: true, command: 'cat')
   ]) {
 
