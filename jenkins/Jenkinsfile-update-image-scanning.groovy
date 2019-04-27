7304710525c782343fb6d65a36936ae445fe99b0
Fabio Gomez <fabiogomez@us.ibm.com>
2019-04-27 18:52:07 -0500

    - Simpler

--- jenkins/Jenkinsfile-update-image-scanning.groovy
+++ jenkins/Jenkinsfile-update-image-scanning.groovy
@@ -73,7 +73,6 @@ podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, envVa
     ],
     volumes: [
         hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')
-        //emptyDirVolume(mountPath: '/var/lib/docker', memory: false)
     ],
     containers: [
         containerTemplate(name: 'nodejs', image: 'ibmcase/nodejs:6-alpine', ttyEnabled: true, command: 'cat'),
@@ -84,53 +83,6 @@ podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, envVa
 
     node(podLabel) {
         checkout scm
-
-        // Local
-        /*container(name:'nodejs', shell:'/bin/bash') {
-            stage('Local - Build and Unit Test') {
-                sh """
-                #!/bin/bash
-                # Go to source directory
-                cd StoreWebApp
-
-                # Install dependencies
-                npm install
-
-                # Perform linting
-                jshint app.js
-                jshint routes/
-
-                cd ..
-                """
-            }
-            stage('Local - Run and Test') {
-                sh """
-                #!/bin/bash
-
-                # Go to source directory
-                cd StoreWebApp
-
-                # Start Application
-                node ./bin/www &
-                PID=`echo \$!`
-
-                # Wait for the Web app to start accepting connections
-                sleep 10
-
-                # Get back to root folder
-                cd ..
-
-                # Let the application start
-                bash scripts/health_check.sh "http://127.0.0.1:${MANAGEMENT_PORT}"
-
-                # Run tests
-                bash scripts/api_tests.sh 127.0.0.1 ${MICROSERVICE_PORT}
-
-                # Kill process
-                kill \${PID}
-                """
-            }
-        }*/
 
         // Docker
         container(name:'docker', shell:'/bin/bash') {
@@ -148,55 +100,7 @@ podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, envVa
                 docker build -t \${IMAGE} .
                 """
             }
-            /*stage('Docker - Run and Test') {
-                sh """
-                #!/bin/bash
 
-                # Get image
-                if [ "${REGISTRY}" == "docker.io" ]; then
-                    IMAGE=${IMAGE_NAME}:${env.BUILD_NUMBER}
-                else
-                    IMAGE=${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${env.BUILD_NUMBER}
-                fi
-
-                # Kill Container if it already exists
-                docker kill ${MICROSERVICE_NAME} || true
-                docker rm ${MICROSERVICE_NAME} || true
-
-                # Start Container
-                echo "Starting ${MICROSERVICE_NAME} container"
-                set +x
-                docker run --name ${MICROSERVICE_NAME} -d \
-                    -p ${MICROSERVICE_PORT}:${MICROSERVICE_PORT} \
-                    -p ${MANAGEMENT_PORT}:${MANAGEMENT_PORT} \
-                    -e SERVICE_PORT=${MICROSERVICE_PORT} \
-                    \${IMAGE}
-                set -x
-
-                # Check that application started successfully
-                docker ps
-
-                # Check the logs
-                docker logs -f ${MICROSERVICE_NAME} &
-                PID=`echo \$!`
-
-                # Get the container IP Address
-                CONTAINER_IP=`docker inspect ${MICROSERVICE_NAME} | jq -r '.[0].NetworkSettings.IPAddress'`
-
-                # Let the application start
-                bash scripts/health_check.sh "http://\${CONTAINER_IP}:${MANAGEMENT_PORT}"
-
-                # Run tests
-                bash scripts/api_tests.sh \${CONTAINER_IP} ${MICROSERVICE_PORT}
-
-                # Kill process
-                kill \${PID}
-
-                # Kill Container
-                docker kill ${MICROSERVICE_NAME} || true
-                docker rm ${MICROSERVICE_NAME} || true
-                """
-            }*/
             stage('Docker - Push Image to Registry') {
                 withCredentials([usernamePassword(credentialsId: registryCredsID,
                                                usernameVariable: 'USERNAME',
@@ -218,114 +122,9 @@ podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, envVa
                 }
             }
         }
-    }
-
-    node("aqua") {
         stage('Aqua MicroScanner') {
             def image = registry + namespace + imageName + currentBuild.number
-            aquaMicroScanner imageName: image, notCompliesCmd: 'exit 1', onDisallowed: 'fail'
-            aquaMicroscanner imageName: image, notCompliesCmd: '', onDisallowed: 'ignore', outputFormat: 'html'
-        }
-    }
-
-    node(podLabel) {
-        // Kubernetes
-        container(name:'kubernetes', shell:'/bin/bash') {
-            stage('Initialize helm') {
-                sh """
-                echo "Initializing Helm ..."
-                export HELM_HOME=${HELM_HOME}
-                helm init -c
-                """
-            }
-            // Initialize cloudctl for IBM Cloud Private
-            if (env.TLS && env.TLS == "--tls") {
-                stage ('Initialize cloudctl') {
-                    withCredentials([usernamePassword(credentialsId: clusterCredentialId,
-                                                   passwordVariable: 'CLUSTER_PASSWORD',
-                                                   usernameVariable: 'CLUSTER_USERNAME')]) {
-                        sh """
-                        echo "Login with cloudctl ..."
-                        cloudctl login -a ${CLUSTER_URL} -u ${CLUSTER_USERNAME}  -p "${CLUSTER_PASSWORD}" -c ${CLUSTER_ACCOUNT_ID} -n ${NAMESPACE} --skip-ssl-validation
-                        """
-                    }
-                }
-            }
-            stage('Kubernetes - Deploy new Docker Image') {
-                sh """
-                #!/bin/bash
-
-                # Get image
-                if [ "${REGISTRY}" == "docker.io" ]; then
-                    IMAGE=${IMAGE_NAME}
-                else
-                    IMAGE=${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}
-                fi
-
-                # Helm Parameters
-                if [ "${DEPLOY_NEW_VERSION}" == "true" ]; then
-                    NAME="${MICROSERVICE_NAME}-v${env.BUILD_NUMBER}"
-                    VERSION_LABEL="--set labels.version=v${env.BUILD_NUMBER}"
-                else
-                    NAME="${MICROSERVICE_NAME}"
-                fi
-
-                echo "Installing chart/${MICROSERVICE_NAME} chart with name \${NAME} and waiting for pods to be ready"
-
-                set +x
-                helm upgrade --install \${NAME} --namespace ${NAMESPACE} \${VERSION_LABEL} \
-                    --set fullnameOverride=\${NAME} \
-                    --set image.repository=\${IMAGE} \
-                    --set image.tag=${env.BUILD_NUMBER} \
-                    --set service.externalPort=${MICROSERVICE_PORT} \
-                    --set services.auth.protocol=${AUTH_PROTOCOL} \
-                    --set services.auth.host=${AUTH_HOST} \
-                    --set services.auth.port=${AUTH_PORT} \
-                    --set services.catalog.protocol=${CATALOG_PROTOCOL} \
-                    --set services.catalog.host=${CATALOG_HOST} \
-                    --set services.catalog.port=${CATALOG_PORT} \
-                    --set services.customer.protocol=${CUSTOMER_PROTOCOL} \
-                    --set services.customer.host=${CUSTOMER_HOST} \
-                    --set services.customer.port=${CUSTOMER_PORT} \
-                    --set services.orders.protocol=${ORDERS_PROTOCOL} \
-                    --set services.orders.host=${ORDERS_HOST} \
-                    --set services.orders.port=${ORDERS_PORT} \
-                    chart/${MICROSERVICE_NAME} --wait ${TLS}
-                set -x
-                """
-            }
-            stage('Kubernetes - Test') {
-                sh """
-                #!/bin/bash
-
-                # Get deployment
-                if [ "${DEPLOY_NEW_VERSION}" == "true" ]; then
-                    QUERY_LABELS="${SERVICE_LABELS},version=v${env.BUILD_NUMBER}"
-                else
-                    QUERY_LABELS="${SERVICE_LABELS}"
-                fi
-
-                DEPLOYMENT=`kubectl --namespace=${NAMESPACE} get deployments -l \${QUERY_LABELS} -o name | head -n 1`
-
-                # Wait for deployment to be ready
-                kubectl --namespace=${NAMESPACE} rollout status \${DEPLOYMENT}
-
-                # Port forwarding & logs
-                kubectl --namespace=${NAMESPACE} port-forward \${DEPLOYMENT} ${MICROSERVICE_PORT} ${MANAGEMENT_PORT} &
-                kubectl --namespace=${NAMESPACE} logs -f \${DEPLOYMENT} &
-                echo "Sleeping for 3 seconds while connection is established..."
-                sleep 3
-
-                # Let the application start
-                bash scripts/health_check.sh "http://127.0.0.1:${MANAGEMENT_PORT}"
-
-                # Run tests
-                bash scripts/api_tests.sh 127.0.0.1 ${MICROSERVICE_PORT}
-
-                # Kill port forwarding
-                killall kubectl || true
-                """
-            }
+            aquaMicroscanner imageName: image, notCompliesCmd: 'exit 1', onDisallowed: 'fail', outputFormat: 'html'
         }
     }
 }
