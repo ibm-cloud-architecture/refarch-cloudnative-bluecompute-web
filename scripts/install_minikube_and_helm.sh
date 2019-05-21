d759a1cb19ab90608519a9160e757832d74c6d4a
dshimo <dshi@utexas.edu>
2019-05-21 16:04:41 -0500

    travis test: updating install script and add some debug lines

--- scripts/install_minikube_and_helm.sh
+++ scripts/install_minikube_and_helm.sh
@@ -3,10 +3,13 @@
 # Install nsenter, which is needed for minikube to work
 bash scripts/install_nsenter.sh
 # Download kubectl, which is a requirement for using minikube.
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+KUBERNETES_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_VERSION/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 # Download minikube.
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-sudo minikube start --vm-driver=none --kubernetes-version=v1.9.0
+sudo minikube start --vm-driver=none --kubernetes-version=$KUBERNETES_VERSION
+# Update minikube direcotry permissions
+sudo chown -R travis: /home/travis/.minikube/
 # Fix the kubectl context, as it's often stale.
 minikube update-context
 # Getting ip for testing
@@ -22,10 +25,12 @@ kubectl -n kube-system create sa tiller && kubectl create clusterrolebinding til
 helm init --service-account tiller
 # Wait for helm to be ready
 until helm list; do echo "waiting for helm to be ready"; sleep 1; done
+# Then show helm version
+helm version
 
 # Add incubator and bluecompute-charts Helm repos
 helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
-helm repo add ibmcase-charts https://raw.githubusercontent.com/ibm-cloud-architecture/refarch-cloudnative-kubernetes/spring/docs/charts
+# helm repo add ibmcase-charts https://raw.githubusercontent.com/ibm-cloud-architecture/refarch-cloudnative-kubernetes/spring/docs/charts
 
 # Get cluster info
 kubectl cluster-info
