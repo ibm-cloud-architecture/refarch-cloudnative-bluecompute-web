d06ce94a40b51f5857efe4e20cbb487c7866f2cd
dshimo <dshi@utexas.edu>
2019-05-22 07:46:54 -0500

    upgrading minikube version 25->35

--- scripts/install_minikube_and_helm.sh
+++ scripts/install_minikube_and_helm.sh
@@ -6,7 +6,7 @@ bash scripts/install_nsenter.sh
 KUBERNETES_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_VERSION/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 # Download minikube.
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.35.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 sudo minikube start --vm-driver=none --kubernetes-version=$KUBERNETES_VERSION
 # Update minikube direcotry permissions
 sudo chown -R travis: /home/travis/.minikube/
