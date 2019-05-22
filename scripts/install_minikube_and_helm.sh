55baf9ed08494314cdc7040493799831e0ec2b1c
dshimo <dshi@utexas.edu>
2019-05-22 15:17:59 -0500

    systemd attempt

--- scripts/install_minikube_and_helm.sh
+++ scripts/install_minikube_and_helm.sh
@@ -8,7 +8,7 @@ echo "$KUBERNETES_VERSION"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_VERSION/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 # Download minikube.
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.1.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-sudo minikube start --vm-driver=none --kubernetes-version=$KUBERNETES_VERSION --bootstrapper=localkube
+sudo minikube start --vm-driver=none --kubernetes-version=$KUBERNETES_VERSION --extra-config=kubelet.cgroup-driver=systemd
 # Update minikube direcotry permissions
 sudo chown -R travis: /home/travis/.minikube/
 # Fix the kubectl context, as it's often stale.
