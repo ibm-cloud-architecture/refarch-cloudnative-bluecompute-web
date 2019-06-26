1e1a22979fb4050d57b843e417b8975e37ca520a
Fabio Gomez <fabiogomez@us.ibm.com>
2019-06-26 13:01:06 -0500

    - Added environment variables back

--- jenkins/Jenkinsfile-podman.groovy
+++ jenkins/Jenkinsfile-podman.groovy
@@ -152,7 +152,13 @@ podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, envVa
                     IMAGE=${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}
                 fi
 
-                NAME="${MICROSERVICE_NAME}"
+                # Helm Parameters
+                if [ "${DEPLOY_NEW_VERSION}" == "true" ]; then
+                    NAME="${MICROSERVICE_NAME}-v${env.BUILD_NUMBER}"
+                    VERSION_LABEL="--set labels.version=v${env.BUILD_NUMBER}"
+                else
+                    NAME="${MICROSERVICE_NAME}"
+                fi
 
                 echo "Installing chart/${MICROSERVICE_NAME} chart with name \${NAME} and waiting for pods to be ready"
 
@@ -183,7 +189,11 @@ podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, envVa
                 #!/bin/bash
 
                 # Get deployment
-                QUERY_LABELS="${SERVICE_LABELS}"
+                if [ "${DEPLOY_NEW_VERSION}" == "true" ]; then
+                    QUERY_LABELS="${SERVICE_LABELS},version=v${env.BUILD_NUMBER}"
+                else
+                    QUERY_LABELS="${SERVICE_LABELS}"
+                fi
 
                 DEPLOYMENT=`kubectl --namespace=${NAMESPACE} get deployments -l \${QUERY_LABELS} -o name | head -n 1`
 
